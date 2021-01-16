7091b4b3410d876be0510722eb17d071302770fb
Antek Kois <koisantek96@gmail.com>
2021-01-16 23:40:06 +0100

    Add some details to project
    Edit a litlle background, and layouts of index.html

--- src/resources/components/project/project.js
+++ src/resources/components/project/project.js
@@ -1,3 +1,4 @@
+const axios = require('axios');
 
 export const addPositionProject = () =>{
     
@@ -10,82 +11,94 @@ export const addPositionProject = () =>{
     
 }
 
-const axios = require('axios');
 
+class Project {
+    constructor(dataOfProject){
+        this.projectWrapper = this.createProjectWrapper();
+        this.project = this.createProject();
+        this.createProjectprojectImage(dataOfProject, this.project);
+        this.createProjectTitle(dataOfProject, this.project);
+        this.createProjectData(dataOfProject, this.project);
+        this.createProjectDescription(dataOfProject, this.project);
+        this.createProjectButton(this.project);
+        this.projectWrapper.appendChild(this.project);
+        return this.projectWrapper;
+    }
+    createProjectWrapper(){
+        const Wrapper = document.createElement('div');
+        Wrapper.classList.add('project__wrapper');
+        return Wrapper;
+    }
+    createProject(){
+        const project = document.createElement('article');
+        project.classList.add('project');
+        return project;
+    }
+    createProjectprojectImage(dataOfProject, project){
+        const projectImageWrapper = document.createElement('div');
+        projectImageWrapper.classList.add('project__imgage-wrapper');
+        const projectImage = document.createElement('img');
+        const src = `./img/iconmonstr-${ dataOfProject.category }.svg`;
+        projectImage.setAttribute('src', src);
+        projectImage.setAttribute('loading', 'lazy');
+        projectImage.setAttribute('alt', 'image representation of project category');
+        projectImage.classList.add('project__image');
+        projectImageWrapper.appendChild(projectImage);
+        project.appendChild(projectImageWrapper);
+    }
+    createProjectTitle(dataOfProject, project){
+        const projectTitle = document.createElement('h3');
+        projectTitle.classList.add('project__title');
+        projectTitle.innerHTML = dataOfProject.title;
+        project.appendChild(projectTitle);
+    }
+    createProjectData(dataOfProject, project){
+        const projectDate = document.createElement('p');
+        projectDate.classList.add('project__date');
+        const date = new Date(dataOfProject.date);
+        const dateToPut = `${date.getDate()} ${date.toLocaleString('default', { month: 'long' })} ${date.getFullYear()}`
+        projectDate.innerHTML = dateToPut;
+        project.appendChild(projectDate);
+    }
+    createProjectDescription(dataOfProject, project){
+        const projectDescription = document.createElement('p');
+        projectDescription.classList.add('project__description')
+        projectDescription.innerHTML = dataOfProject.description;
+        project.appendChild(projectDescription);
+    }
+    createProjectButton(project){
+        const buttonWrapper = document.createElement('div');
+        buttonWrapper.classList.add('project__button__wrpapper');
 
-const createButton = () =>{
-    const buttonWrapper = document.createElement('div');
-    buttonWrapper.classList.add('project__button__wrpapper');
+        const button = document.createElement('div');
+        button.classList.add('project__button');
 
-    const button = document.createElement('div');
-    button.classList.add('project__button');
+        const rightButton = document.createElement('button');
+        rightButton.classList.add('button');
+        rightButton.classList.add('button--project');
 
-    const rightButton = document.createElement('button');
-    rightButton.classList.add('button');
-    rightButton.classList.add('button--project');
+        const span = document.createElement('a');
+        span.innerHTML = 'Wszystkie projekty';
+        span.classList.add('button__link');
+        span.setAttribute('data-text', 'Wszystkie projekty');
+        span.setAttribute('href', 'http://localhost:8081/posts');
 
-    const span = document.createElement('a');
-    span.innerHTML = 'Wszystkie projekty';
-    span.classList.add('button__link');
-    span.setAttribute('data-text', 'Wszystkie projekty');
-    span.setAttribute('href', 'http://localhost:8081/posts');
+        const butttonIcon = document.createElement('div');
+        butttonIcon.classList.add('button__icon');
 
-    const butttonIcon = document.createElement('div');
-    butttonIcon.classList.add('button__icon');
+        span.appendChild(butttonIcon);
+        rightButton.appendChild(span);
+        button.appendChild(rightButton);
+        buttonWrapper.appendChild(button);
 
-    span.appendChild(butttonIcon);
-    rightButton.appendChild(span);
-    button.appendChild(rightButton);
-    buttonWrapper.appendChild(button);
-
-    return buttonWrapper;
+        project.appendChild(buttonWrapper);
+    }
 };
-
-const createOneProject = (dataOfProject) =>{
-    const projectWrapper = document.createElement('div');
-    projectWrapper.classList.add('project__wrapper');
-
-    const project = document.createElement('article');
-    project.classList.add('project');
-
-    const projectImageWrapper = document.createElement('div');
-    projectImageWrapper.classList.add('project__imgage-wrapper');
-    const projectImage = document.createElement('img');
-    const src = `./img/iconmonstr-${ dataOfProject.category }.svg`;
-    projectImage.setAttribute('src', src);
-    projectImage.setAttribute('loading', 'lazy');
-    projectImage.setAttribute('alt', 'image representation of project category');
-    projectImage.classList.add('project__image');
-    projectImageWrapper.appendChild(projectImage);
-    project.appendChild(projectImageWrapper);
-
-    const projectTitle = document.createElement('h3');
-    projectTitle.classList.add('project__title');
-    projectTitle.innerHTML = dataOfProject.title;
-    project.appendChild(projectTitle);
-
-    const projectDate = document.createElement('p');
-    projectDate.classList.add('project__date');
-    projectDate.innerHTML = dataOfProject.date;
-    project.appendChild(projectDate);
-
-    const projectDescription = document.createElement('p');
-    projectDescription.classList.add('project__description')
-    projectDescription.innerHTML = dataOfProject.description;
-    project.appendChild(projectDescription);
-
-    const projectButton = createButton();
-    project.appendChild(projectButton);
-    
-    projectWrapper.appendChild(project);
-
-    return projectWrapper;
-}
 
 const createProjects = (data) => {
     const fragment = document.createDocumentFragment();
     data.forEach(element => {
-        const project = createOneProject(element);
+        const project = new Project(element);
         fragment.appendChild(project);
     })
     return fragment;
@@ -93,8 +106,8 @@ const createProjects = (data) => {
 
 const getAllPosts =  async () =>{
     try {
-        const { data } = await  axios.get('http://localhost:8081/posts');
-        const allProject =createProjects(data);
+        const { data } = await  axios.get('http://localhost:8081/posts?limit=4');
+        const allProject = createProjects(data);
         document.querySelector('.projects-wrapper').appendChild(allProject);
         addPositionProject();
     } catch (error) {
@@ -104,5 +117,3 @@ const getAllPosts =  async () =>{
 };
 
 document.onload = getAllPosts();
-//const allButtons = document.querySelectorAll('.button--project .button__link');
-//allButtons.forEach(button => button.addEventListener('click', getAllPosts));
